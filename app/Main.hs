8089fab8f1eb8b080a376b9fc3a15c4f4fda60dc
Jor Bashllari <altnate2000@gmail.com>
2020-07-24 22:20:24 +0200

    use only app/Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
+
 module Main where
 
 import XMonad
@@ -5,25 +7,42 @@ import XMonad
 import XMonad.Layout.Fullscreen (fullscreenSupport)
 import XMonad.Hooks.EwmhDesktops (ewmh)
 import XMonad.Hooks.ManageDocks (docks)
+import XMonad.Layout.NoBorders (borderEventHook)
 
-import Keys (keys')
+-- import qualified DBus as D
+-- import qualified DBus.Client as D
+
+import Keys (keys', mouseBindings', logHook')
 import Layouts (layoutHook')
 import WSRules (manageHook')
 import Utils (startupHook')
 
-opts = def
-  { terminal    = "alacritty"
-  , modMask     = mod4Mask
-  , borderWidth = 0
-  , workspaces  = [ "ffox", "term", "files", "random", "aux", "media" ]
-  , manageHook  = manageHook'
-  , startupHook = startupHook'
-  , layoutHook  = layoutHook'
-  , keys        = keys'
-  }
+import Config
 
-main = xmonad
-  . fullscreenSupport
-  . ewmh
-  . docks
-  $ opts
+main :: IO ()
+main = do
+
+  -- dbus <- D.connectSession
+  -- -- Request access to the DBus name
+  -- D.requestName dbus (D.busName_ "org.xmonad.Log")
+  --     [D.nameAllowReplacement, D.nameReplaceExisting, D.nameDoNotQueue]
+
+  xmonad opts
+
+
+opts = ewmh
+     . docks
+     . fullscreenSupport
+     $ def
+       { terminal         = "alacritty"
+       , modMask          = mod4Mask
+       , borderWidth      = 4
+       , workspaces       = snd <$> allWorkspaces
+       , keys             = keys'
+       , manageHook       = manageHook'
+       , startupHook      = startupHook'
+       , layoutHook       = layoutHook'
+       , logHook          = logHook'
+       , mouseBindings    = mouseBindings'
+       , handleEventHook  = borderEventHook <> handleEventHook def
+       }
